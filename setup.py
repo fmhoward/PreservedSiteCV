c8735e2a2ac4d61035ab63b560c18de111c063a6
fmhoward <37817972+fmhoward@users.noreply.github.com>
2021-01-19 23:34:22 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ VERSION = '1.0.0'
 DESCRIPTION = 'Preserved Site Cross Validation'
 LONG_DESCRIPTION = 'Provides optimal stratification while preserving sites for cross validation'
 
-# Setting up
 setup(
-    # the name must match the folder name 'verysimplemodule'
     name="preservedsite",
     version=VERSION,
     author="Frederick Howard",
@@ -14,9 +12,7 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=["pandas", "numpy", "cvxpy", "cplex"],  # add any additional packages that
-    # needs to be installed along with your package. Eg: 'caer'
-
+    install_requires=["pandas", "numpy", "cvxpy", "cplex"], 
     keywords=['python', 'cross validation'],
     classifiers=[
         "Development Status :: 2 - Beta",
