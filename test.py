66b182df679823a0b611749d6e2b0771748d40df
fmhoward <37817972+fmhoward@users.noreply.github.com>
2021-03-10 16:16:20 -0600

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ import preservedsite.crossfolds as cv
 import pandas as pd
 
 data = pd.read_csv("example.csv", dtype=str)
-data = cv.generate(data, "feature", ["A", "B"], crossfolds=3, patient_column='patient', site_column='site')
+data = cv.generate(data, "feature", ["A", "B"], crossfolds=3, patient_column='patient', site_column='site', randomseed=0)
